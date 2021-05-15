9091dc9b6c208fa94bdd3c44da1b00312ab68c47
darkorockk <shadrin.dev@gmail.com>
2021-05-15 19:11:13 +0300

    3. Загрузка базы спамеров. [#479077]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -20,7 +20,12 @@ public class ImportDB {
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            /* rd.lines().forEach(...); */
+             rd.lines().forEach(line -> {
+                 String[] arg = line.split(";");
+                 if (arg.length ==2) {
+                     users.add(new User(arg[0], arg[1]));
+                 }
+             });
         }
         return users;
     }
@@ -33,9 +38,10 @@ public class ImportDB {
                 cfg.getProperty("jdbc.password")
         )) {
             for (User user : users) {
-                try (PreparedStatement ps = cnt.prepareStatement("insert info users ...")) {
+                try (PreparedStatement ps = cnt.prepareStatement(
+                        "insert into users(name, email) values (?, ?)")) {
                     ps.setString(1, user.name);
-                    ps.setString(1, user.email);
+                    ps.setString(2, user.email);
                     ps.execute();
                 }
             }
